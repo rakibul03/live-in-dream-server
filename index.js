1147820fbe4f07ffe159bbb6fa1dc8d1cdba9d80
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2022-12-09 15:38:08 +0530

    Create jwt token for user

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
@@ -21,8 +22,27 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const homesCollection = client.db("liveInDream").collection("homes");
+    const usersCollection = client.db("liveInDream").collection("users");
 
-    console.log("Database Connected...");
+    // Save user email and generate JWT
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1d",
+      });
+      res.send({ result, token });
+    });
   } finally {
   }
 }
